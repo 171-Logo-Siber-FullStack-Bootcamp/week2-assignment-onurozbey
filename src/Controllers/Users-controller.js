46739a45e77dd4ddbdc245265292dbd401763788
Onur Özbey <51442632+onurozbey@users.noreply.github.com>
2022-02-12 17:38:53 +0300

    Update Users-controller.js

--- src/Controllers/Users-controller.js
+++ src/Controllers/Users-controller.js
@@ -1,11 +1,14 @@
 const users = require("../Data/Users");
 
 getAllUsers = () => {
+  // Bütün verileri döndür
   return users;
 };
 
 getUserByID = (UserID) => {
+  // ID'si belirtilen veriyi döndür
   return users.filter((x) => x.id == UserID);
 };
 
+//Belirtilen metotları dışarıdan erişilebilir duruma getir
 module.exports = { getAllUsers, getUserByID };
