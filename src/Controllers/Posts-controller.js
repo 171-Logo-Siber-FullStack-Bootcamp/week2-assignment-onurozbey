d95c865c7cba481e3e8c1fe8eb4b17a805a1a8f5
Onur Özbey <51442632+onurozbey@users.noreply.github.com>
2022-02-12 17:37:42 +0300

    Update Posts-controller.js

--- src/Controllers/Posts-controller.js
+++ src/Controllers/Posts-controller.js
@@ -1,11 +1,14 @@
 const posts = require("../Data/Posts");
 
 getAllPosts = () => {
+  // Bütün verileri döndür
   return posts;
 };
 
 getPostByID = (postID) => {
+  // ID'si belirtilen veriyi döndür
   return posts.filter((x) => x.id == postID);
 };
 
+//Belirtilen metotları dışarıdan erişilebilir duruma getir
 module.exports = { getAllPosts, getPostByID };
